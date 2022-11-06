cfe8a5da7ec1442a573624e7b8f874ab3f89eaf1
Daniel <danel.filipek@gmail.com>
2022-11-06 14:57:19 +0100

    Init commit

---
+++ src/main/java/com/qsspy/graphtool/GraphToolApplication.java
@@ -0,0 +1,13 @@
+package com.qsspy.graphtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsspy/graphtool/GraphToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsspy.graphtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
